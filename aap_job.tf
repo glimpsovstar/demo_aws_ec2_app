d752d0d701e0a7da6af138d6deb8b24056a9b9fc
Aaron Evans <aarone@outlook.com.au>
2025-06-12 12:01:23 +1000

    Add aap_group and aap_host resources to manage VM inventory in AAP

--- aap_job.tf
+++ aap_job.tf
@@ -4,3 +4,18 @@ resource "aap_inventory" "vm_inventory" {
   description = "Inventory for VMs built with HCP Terraform and managed by AAP"
 }
 
+resource "aap_group" "vm_groups" {
+  inventory_id = aap_inventory.vm_inventory.id
+  name         = "vm_groups"
+}
+
+resource "aap_host" "sample_foo" {
+  inventory_id = aap_inventory.vm_inventory.id
+  name         = "tf_host_foo"
+  variables = jsonencode(
+    {
+      "foo" : "bar"
+    }
+  )
+  groups = [aap_group.vm_groups.id]
+}
