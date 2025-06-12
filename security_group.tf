43122ede16fe526336510b36c3c4e66081c62e33
Aaron Evans <aarone@outlook.com.au>
2025-06-12 11:35:55 +1000

    Update SSH ingress rule CIDR block to restrict access to specific IP range for improved security.

--- security_group.tf
+++ security_group.tf
@@ -41,7 +41,7 @@ resource "aws_security_group_rule" "ssh_ingress" {
   from_port   = 22
   to_port     = 22
   protocol    = "tcp"
-  cidr_blocks = ["10.10.20.0/0"]
+  cidr_blocks = ["192.168.0.0/24"]
 
   lifecycle {
     create_before_destroy = true
