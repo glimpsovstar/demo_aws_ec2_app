5e50a044e9c7f0979a72e7086e950461320205a7
David Joo <glimpsovstar@gmail.com>
2025-09-25 12:24:19 +1000

    Update default EC2 instance type to t3.micro in variables.tf

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "instance_name_prefix" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.small"
+  default     = "t3.micro"
 }
 
 variable "ami_id" {
